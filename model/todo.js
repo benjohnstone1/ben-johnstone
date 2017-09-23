e268610fe39830edf5ba1233068c837994f61de4
Ben Johnstone <benji.forrest@gmail.com>
2017-09-23 04:09:02 +0000

    connected mongodb and updated todo.js

--- model/todo.js
+++ model/todo.js
@@ -1,24 +1,6 @@
 // define model =================
 var mongoose = require('mongoose');
-
-var uri = 'mongodb://heroku_n2k302mx:u2m8korq51b25f743m1haml9u8@ds161873.mlab.com:61873/heroku_n2k302mx';
-
-mongoose.connect(uri);
-
-var db = mongoose.createConnection(uri, {
-  useMongoClient: true,
-  /* other options */
+var todoSchema = new mongoose.Schema({
+    text: String
 });
-
-
-db.on('error', console.error.bind(console, 'connection error:'));
-
-db.once('open', function callback () {
-
-
-    var todoSchema = new mongoose.Schema({
-        text: String
-    });
-
-    mongoose.model('Todo', todoSchema);
-});
+mongoose.model('Todo', todoSchema);
