aa0b506fce0c4c2135b1407c883e35b6e10bc1d9
Ben Johnstone <benji.forrest@gmail.com>
2017-10-04 23:37:20 +0000

    removed redundant files

--- public/javascripts/controllers/authentication.controller.js
+++
@@ -1,3 +0,0 @@
-var passport = require('passport');
-var mongoose = require('mongoose');
-var User = mongoose.model('User');
--- public/javascripts/core.js
+++
@@ -1,9 +0,0 @@
-/* global angular */
-// angular.module('myApp', ['todoController', 'todoService', 'loginController', 'loginService', 'accountsController', 'accountsService', 'profileController']);
-// angular.module('myApp', ['accountsController', 'AccountsService']);
-// angular.module('myApp', ['profileController', 'loginService']);
-// angular.module('myApp', ['loginController', 'loginService']);
-
-angular.module('myApp', []).controller('homeController', ['$scope', function($scope) {
-    $scope.welcome = 'Home... Welcome :)';
-}]);
