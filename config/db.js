accd7f9618f931909d10ab670c8f34a2c4ed09ff
Ben Johnstone <benji.forrest@gmail.com>
2017-11-09 03:17:03 +0000

    updates...

--- config/db.js
+++ config/db.js
@@ -1,13 +1,12 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+// Connect to mLab Database
 var uri = 'mongodb://heroku_n2k302mx:u2m8korq51b25f743m1haml9u8@ds161873.mlab.com:61873/heroku_n2k302mx';
 // connect to db in command line
 // $ mongo ds161873.mlab.com:61873/heroku_n2k302mx -u heroku_n2k302mx -p u2m8korq51b25f743m1haml9u8
 
-//var uri = 'mongodb://localhost/nodewebappdb'
 mongoose.connect(uri, {
   useMongoClient: true,
-  /* other options */
 });
 
 var db = mongoose.connection;
@@ -15,6 +14,6 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   // We're connected!
   console.log('We are connected to DB!');
-  console.log('Test Enivronment: http://express-crud-benjiforrest.c9users.io');
+  console.log('Test Enivronment: https://express-crud-benjiforrest.c9users.io');
 });
 
